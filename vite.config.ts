243e205a8755fe5d02e71d24eb273ca290e1737b
David Warrington <davidmwarrington+github@gmail.com>
2023-02-26 19:41:02 +0000

    feat(types): generate type defs on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: './src/main.ts',
+      entry: './src/translate.ts',
       name: 'Translate',
       fileName: 'translate',
     },
   },
+  plugins: [dts()],
 });
